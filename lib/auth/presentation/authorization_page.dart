8519b9587ea1fc87434a22840ef4441d6eccb8c5
Dmitry <b4r4n0v@gmail.com>
2023-01-28 15:28:16 +0200

    feat: add navigation, splash screen, sign in screen and starred repos screen container

---
+++ lib/auth/presentation/authorization_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AuthorizationPage extends StatefulWidget {
+  final Uri authorizationUrl;
+  final void Function(Uri redirectUrl) onAuthorizationCodeRedirectAttempt;
+
+  const AuthorizationPage({
+    super.key,
+    required this.authorizationUrl,
+    required this.onAuthorizationCodeRedirectAttempt,
+  });
+
+  @override
+  State<AuthorizationPage> createState() => _AuthorizationPageState();
+}
+
+class _AuthorizationPageState extends State<AuthorizationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
